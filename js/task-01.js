ab28ed22d660d1ba86941cd0a6ee1093762f973d
KonstantinBYC <globalmacintosh@gmail.com>
2023-07-06 23:24:13 +0300

    edit 06-07-23

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,12 @@
 const listOfCategories = document.querySelectorAll(".item");
 
-console.log(`Number of categories: ${listOfCategories.length}`);
+console.log(`Number of categories:`, listOfCategories.length);
 
 listOfCategories.forEach((element) =>
   console.log(
-    `Category: ${element.firstElementChild.textContent}`,
-    `Elements: ${element.lastElementChild.children.length}`
+    `Category:`,
+    element.firstElementChild.textContent,
+    `Elements:`,
+    element.lastElementChild.children.length
   )
 );
-
-// const animals = document.querySelector("categories.firstChild.children");
-// const products = document.querySelector("categories.secondChild.children");
-// const technologies = document.querySelector("categories.thirdChild.children");
-
-// console.log(`Category: ${categories.firstChild}`);
-// console.log(`Elements: ${animals.length};`);
-// console.log(`Category: ${categories.secondChild}`);
-// console.log(`Elements: ${products.length};`);
-// console.log(`Category: ${categories.thirdChild}`);
-// console.log(`Elements: ${technologies.length};`);
